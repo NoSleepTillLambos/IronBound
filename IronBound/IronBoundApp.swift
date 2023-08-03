09642172e36c2a2637e0f24094205610fb19a2f5
Liam Wedge <21100218@virtualwindow.co.za>
2023-08-03 11:08:48 +0200

    Initial Commit

---
+++ IronBound/IronBoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  IronBoundApp.swift
+//  IronBound
+//
+//  Created by Paige Phelps on 2023/08/03.
+//
+
+import SwiftUI
+
+@main
+struct IronBoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
