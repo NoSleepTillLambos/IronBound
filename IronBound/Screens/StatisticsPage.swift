ecd4c06462e0095e2945195bbfc947bc4abb314d
Liam Wedge <21100218@virtualwindow.co.za>
2023-09-02 11:20:49 +0200

    home page completed

--- IronBound/Screens/StatisticsPage.swift
+++ IronBound/Screens/StatisticsPage.swift
@@ -18,7 +18,7 @@ struct StatisticsPage: View {
         NavigationView {
             
             ZStack {
-                
+                Color("C9BFDF").ignoresSafeArea()
                 ScrollView(.vertical, showsIndicators: false) {
                     
                     LazyVGrid(columns: items, spacing: 2) {
@@ -28,9 +28,11 @@ struct StatisticsPage: View {
                                 VStack {
                                     Text(Activity.image)
                                         .frame(width: 50, height: 50)
-                                        .background(RoundedRectangle(cornerRadius: 16).fill(Color("46748B").opacity(0.5)))
+                                        .background(RoundedRectangle(cornerRadius: 16).fill(Color("Light").opacity(0.5)))
                                     
                                     Text(Activity.name).foregroundColor(Color.white)
+                                    
+                                    
                                 }
                                 .padding()
                                 .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -38,9 +40,40 @@ struct StatisticsPage: View {
                             }.buttonStyle(PlainButtonStyle())
                         }
                     }.padding()
+                    VStack(alignment: .center, spacing: 10) {
+                            
+                                Text("Keep Fit")
+                                    .multilineTextAlignment(.center)
+                                    .font(.system(size: 18.0))
+                                    .foregroundColor(.white)
+                        
+                            }.frame(width: 320,height: 250, alignment: .center) //<-- Here
+                        .padding(12)
+                        .cornerRadius(20)
+                        .background(Image("gym-home").resizable().scaledToFit().overlay(Color.gray.opacity(0.4)).cornerRadius(16).frame(width: 330.0, alignment: .top))
+                    VStack(alignment: .center, spacing: 10) {
+                            
+                                Text("Track")
+                                    .multilineTextAlignment(.center)
+                                    .font(.system(size: 18.0))
+                                    .foregroundColor(.white)
+                        
+                            }.frame(width: 320,height: 250, alignment: .center) //<-- Here
+                        .padding(12)
+                        .cornerRadius(20)
+                        .background(Image("track").resizable().scaledToFit().overlay(Color.gray.opacity(0.4)).cornerRadius(16).frame(width: 380.0, alignment: .top))
                 }
-                Text("Hey")
-            }.navigationTitle("Health Statistics")
+                
+            }.navigationTitle("Health Statistics").navigationBarTitleDisplayMode(.inline).toolbar {
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        
+                        Text("Health Statistics")
+                            .font(.headline)
+                            .foregroundColor(Color("111009"))
+                    }
+                }
+            }
         }.onAppear {
             repository.requestAuth {
                 success in
